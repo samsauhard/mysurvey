cbf26145e6ccc0a1b870ee1d64898888dc2ff63c
Sauhard <samsauhard1@gmail.com>
2019-04-19 22:56:13 -0400

    Changes Latest

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -13,8 +13,6 @@ import { HomeComponent } from './pages/home/home.component';
 import { SurveysComponent } from './pages/surveys/surveys.component';
 import { AboutComponent } from './pages/about/about.component';
 import { PagenotfoundComponent } from './pages/pagenotfound/pagenotfound.component';
-import { ContactDeleteComponent } from './contacts/contact-delete/contact-delete.component';
-import { ContactDetailsComponent } from './contacts/contact-details/contact-details.component';
 
 import { FlashMessagesModule, FlashMessagesService } from 'angular2-flash-messages';
 import { AuthService } from './services/auth.service';
@@ -41,8 +39,6 @@ export function jwtTokenGetter() {
     AboutComponent,
     SurveysComponent,
     PagenotfoundComponent,
-    ContactDeleteComponent,
-    ContactDetailsComponent,
     RegisterComponent,
     LoginComponent,
     SurveyAnsComponent
